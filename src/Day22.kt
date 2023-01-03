789a2a027ea57954301d7267a14e26e39bfbc3c7
wgolyakov <wolodia@narod.ru>
2023-01-03 18:18:32 +0300

    Day 22 (Universal solution)

--- src/Day22.kt
+++ src/Day22.kt
@@ -1,10 +1,21 @@
+import javafx.geometry.Point3D
+import javafx.scene.transform.Rotate
+import javafx.scene.transform.Transform
 import kotlin.math.min
+import kotlin.math.roundToInt
 
 fun main() {
 	val right = 0
 	val down = 1
 	val left = 2
 	val up = 3
+
+	val positionToMove = mapOf(
+		right to (1 to 0),
+		down to (0 to 1),
+		left to (-1 to 0),
+		up to (0 to -1),
+	)
 
 	fun parseMap(input: List<String>): List<String> {
 		val maxSize = input.take(input.size - 2).maxOf { it.length }
@@ -47,13 +58,7 @@ fun main() {
 					val next = min(l, r)
 					val distance = path.substring(p ,next).toInt()
 					for (m in 0 until distance) {
-						val (dx, dy) = when (facing) {
-							right -> 1 to 0
-							down -> 0 to 1
-							left -> -1 to 0
-							up -> 0 to -1
-							else -> error("Unknown facing: $facing")
-						}
+						val (dx, dy) = positionToMove[facing]!!
 						var t = move1(map, x, y, dx, dy)
 						while (map[t.second][t.first] == ' ')
 							t = move1(map, t.first, t.second, dx, dy)
@@ -71,264 +76,117 @@ fun main() {
 		return (y + 1) * 1000 + (x + 1) * 4 + facing
 	}
 
-	class Face(val num: Int) {
+	class Face(val cx: Int, val cy: Int) {
 		val neighbours = Array(4) { this }
-		var cx = -1
-		var cy = -1
-		var xTransform = Array<(Int, Int) -> Int>(4) { { x, y -> x } }
-		var yTransform = Array<(Int, Int) -> Int>(4) { { x, y -> y } }
-		var facingTransform = IntArray(4) { it }
+		var vector = Point3D(0.0, 0.0, 0.0)
+		var transform: Transform = Rotate()
+		var facing = IntArray(4) { it }
 	}
 
-	fun createTestCube(): List<Face> {
-		val a = 4
-		val cube = List(6) { Face(it) }
+	val reverse = mapOf(
+		right to left,
+		down to up,
+		left to right,
+		up to down,
+	)
 
-		cube[0].cx = 2
-		cube[0].cy = 0
-		cube[0].neighbours[right] = cube[1]
-		cube[0].facingTransform[right] = left
-		cube[0].xTransform[right] = { x, y -> a * 4 - 1 }
-		cube[0].yTransform[right] = { x, y -> a * 3 - 1 - y }
-		cube[0].neighbours[down] = cube[4]
-		cube[0].facingTransform[down] = down
-		cube[0].xTransform[down] = { x, y -> x }
-		cube[0].yTransform[down] = { x, y -> y + 1 }
-		cube[0].neighbours[left] = cube[3]
-		cube[0].facingTransform[left] = down
-		cube[0].xTransform[left] = { x, y -> a + y }
-		cube[0].yTransform[left] = { x, y -> a }
-		cube[0].neighbours[up] = cube[5]
-		cube[0].facingTransform[up] = down
-		cube[0].xTransform[up] = { x, y -> a - 1 - (x - a * 2) }
-		cube[0].yTransform[up] = { x, y -> a }
+	val rotation = mapOf(
+		right to Rotate(-90.0, Rotate.Y_AXIS),
+		left to Rotate(90.0, Rotate.Y_AXIS),
+		up to Rotate(-90.0, Rotate.X_AXIS),
+		down to Rotate(90.0, Rotate.X_AXIS),
+	)
 
-		cube[1].cx = 3
-		cube[1].cy = 2
-		cube[1].neighbours[right] = cube[0]
-		cube[1].facingTransform[right] = left
-		cube[1].xTransform[right] = { x, y -> a * 3 - 1 }
-		cube[1].yTransform[right] = { x, y -> a - 1 - (y - a * 2) }
-		cube[1].neighbours[down] = cube[5]
-		cube[1].facingTransform[down] = right
-		cube[1].xTransform[down] = { x, y -> 0 }
-		cube[1].yTransform[down] = { x, y -> a * 2 - 1 - (x - a * 3) }
-		cube[1].neighbours[left] = cube[2]
-		cube[1].facingTransform[left] = left
-		cube[1].xTransform[left] = { x, y -> x - 1 }
-		cube[1].yTransform[left] = { x, y -> y }
-		cube[1].neighbours[up] = cube[4]
-		cube[1].facingTransform[up] = left
-		cube[1].xTransform[up] = { x, y -> a - 1 - (x - a * 2) }
-		cube[1].yTransform[up] = { x, y -> a }
+	fun rotateTransform(transform: Transform, facing: Int) = transform.createConcatenation(rotation[facing]!!)
 
-		cube[2].cx = 2
-		cube[2].cy = 2
-		cube[2].neighbours[right] = cube[1]
-		cube[2].facingTransform[right] = right
-		cube[2].xTransform[right] = { x, y -> x + 1 }
-		cube[2].yTransform[right] = { x, y -> y }
-		cube[2].neighbours[down] = cube[5]
-		cube[2].facingTransform[down] = up
-		cube[2].xTransform[down] = { x, y -> a * 3 - 1 - x }
-		cube[2].yTransform[down] = { x, y -> a * 2 - 1 }
-		cube[2].neighbours[left] = cube[3]
-		cube[2].facingTransform[left] = up
-		cube[2].xTransform[left] = { x, y -> a * 2 - 1 - (y - a * 2) }
-		cube[2].yTransform[left] = { x, y -> a * 2 - 1 }
-		cube[2].neighbours[up] = cube[4]
-		cube[2].facingTransform[up] = up
-		cube[2].xTransform[up] = { x, y -> x }
-		cube[2].yTransform[up] = { x, y -> y - 1 }
+	val epsilon = 1.0e-5
 
-		cube[3].cx = 1
-		cube[3].cy = 1
-		cube[3].neighbours[right] = cube[4]
-		cube[3].facingTransform[right] = right
-		cube[3].xTransform[right] = { x, y -> x + 1 }
-		cube[3].yTransform[right] = { x, y -> y }
-		cube[3].neighbours[down] = cube[2]
-		cube[3].facingTransform[down] = right
-		cube[3].xTransform[down] = { x, y -> a * 2 }
-		cube[3].yTransform[down] = { x, y -> a * 2 + (a * 2 - 1 - x) }
-		cube[3].neighbours[left] = cube[5]
-		cube[3].facingTransform[left] = left
-		cube[3].xTransform[left] = { x, y -> x - 1 }
-		cube[3].yTransform[left] = { x, y -> y }
-		cube[3].neighbours[up] = cube[0]
-		cube[3].facingTransform[up] = right
-		cube[3].xTransform[up] = { x, y -> a * 2 }
-		cube[3].yTransform[up] = { x, y -> x - a }
-
-		cube[4].cx = 2
-		cube[4].cy = 1
-		cube[4].neighbours[right] = cube[1]
-		cube[4].facingTransform[right] = down
-		cube[4].xTransform[right] = { x, y -> a * 4 - 1 - (y - a) }
-		cube[4].yTransform[right] = { x, y -> a * 2 }
-		cube[4].neighbours[down] = cube[2]
-		cube[4].facingTransform[down] = down
-		cube[4].xTransform[down] = { x, y -> x }
-		cube[4].yTransform[down] = { x, y -> y + 1 }
-		cube[4].neighbours[left] = cube[3]
-		cube[4].facingTransform[left] = left
-		cube[4].xTransform[left] = { x, y -> x - 1 }
-		cube[4].yTransform[left] = { x, y -> y }
-		cube[4].neighbours[up] = cube[0]
-		cube[4].facingTransform[up] = up
-		cube[4].xTransform[up] = { x, y -> x }
-		cube[4].yTransform[up] = { x, y -> y - 1 }
-
-		cube[5].cx = 0
-		cube[5].cy = 1
-		cube[5].neighbours[right] = cube[3]
-		cube[5].facingTransform[right] = right
-		cube[5].xTransform[right] = { x, y -> x + 1 }
-		cube[5].yTransform[right] = { x, y -> y }
-		cube[5].neighbours[down] = cube[2]
-		cube[5].facingTransform[down] = up
-		cube[5].xTransform[down] = { x, y -> a * 3 - 1 - x }
-		cube[5].yTransform[down] = { x, y -> a * 3 - 1 }
-		cube[5].neighbours[left] = cube[1]
-		cube[5].facingTransform[left] = up
-		cube[5].xTransform[left] = { x, y -> a * 4 - 1 - (y - a) }
-		cube[5].yTransform[left] = { x, y -> a * 3 - 1 }
-		cube[5].neighbours[up] = cube[0]
-		cube[5].facingTransform[up] = down
-		cube[5].xTransform[up] = { x, y -> a * 3 - 1 - x }
-		cube[5].yTransform[up] = { x, y -> 0 }
-
-		return cube
+	fun round(p: Point3D): Point3D {
+		val x = if (-epsilon < p.x && p.x < epsilon) 0.0 else p.x
+		val y = if (-epsilon < p.y && p.y < epsilon) 0.0 else p.y
+		val z = if (-epsilon < p.z && p.z < epsilon) 0.0 else p.z
+		return Point3D(x, y, z)
 	}
 
-	fun createCube(): List<Face> {
-		val a = 50
-		val cube = List(6) { Face(it) }
-
-		cube[0].cx = 1
-		cube[0].cy = 0
-		cube[0].neighbours[right] = cube[1]
-		cube[0].facingTransform[right] = right
-		cube[0].xTransform[right] = { x, y -> x + 1 }
-		cube[0].yTransform[right] = { x, y -> y }
-		cube[0].neighbours[down] = cube[4]
-		cube[0].facingTransform[down] = down
-		cube[0].xTransform[down] = { x, y -> x }
-		cube[0].yTransform[down] = { x, y -> y + 1 }
-		cube[0].neighbours[left] = cube[3]
-		cube[0].facingTransform[left] = right
-		cube[0].xTransform[left] = { x, y -> 0 }
-		cube[0].yTransform[left] = { x, y -> a * 3 - 1 - y }
-		cube[0].neighbours[up] = cube[5]
-		cube[0].facingTransform[up] = right
-		cube[0].xTransform[up] = { x, y -> 0 }
-		cube[0].yTransform[up] = { x, y -> a * 3 + (x - a) }
-
-		cube[1].cx = 2
-		cube[1].cy = 0
-		cube[1].neighbours[right] = cube[2]
-		cube[1].facingTransform[right] = left
-		cube[1].xTransform[right] = { x, y -> a * 2 - 1 }
-		cube[1].yTransform[right] = { x, y -> a * 3 - 1 - y }
-		cube[1].neighbours[down] = cube[4]
-		cube[1].facingTransform[down] = left
-		cube[1].xTransform[down] = { x, y -> a * 2 - 1 }
-		cube[1].yTransform[down] = { x, y -> a + (x - a * 2) }
-		cube[1].neighbours[left] = cube[0]
-		cube[1].facingTransform[left] = left
-		cube[1].xTransform[left] = { x, y -> x - 1 }
-		cube[1].yTransform[left] = { x, y -> y }
-		cube[1].neighbours[up] = cube[5]
-		cube[1].facingTransform[up] = up
-		cube[1].xTransform[up] = { x, y -> x - a * 2 }
-		cube[1].yTransform[up] = { x, y -> a * 4 - 1 }
-
-		cube[2].cx = 1
-		cube[2].cy = 2
-		cube[2].neighbours[right] = cube[1]
-		cube[2].facingTransform[right] = left
-		cube[2].xTransform[right] = { x, y -> a * 3 - 1 }
-		cube[2].yTransform[right] = { x, y -> a - 1 - (y - a * 2) }
-		cube[2].neighbours[down] = cube[5]
-		cube[2].facingTransform[down] = left
-		cube[2].xTransform[down] = { x, y -> a - 1 }
-		cube[2].yTransform[down] = { x, y -> a * 3 + (x - a) }
-		cube[2].neighbours[left] = cube[3]
-		cube[2].facingTransform[left] = left
-		cube[2].xTransform[left] = { x, y -> x - 1 }
-		cube[2].yTransform[left] = { x, y -> y }
-		cube[2].neighbours[up] = cube[4]
-		cube[2].facingTransform[up] = up
-		cube[2].xTransform[up] = { x, y -> x }
-		cube[2].yTransform[up] = { x, y -> y - 1 }
-
-		cube[3].cx = 0
-		cube[3].cy = 2
-		cube[3].neighbours[right] = cube[2]
-		cube[3].facingTransform[right] = right
-		cube[3].xTransform[right] = { x, y -> x + 1 }
-		cube[3].yTransform[right] = { x, y -> y }
-		cube[3].neighbours[down] = cube[5]
-		cube[3].facingTransform[down] = down
-		cube[3].xTransform[down] = { x, y -> x }
-		cube[3].yTransform[down] = { x, y -> y + 1 }
-		cube[3].neighbours[left] = cube[0]
-		cube[3].facingTransform[left] = right
-		cube[3].xTransform[left] = { x, y -> a }
-		cube[3].yTransform[left] = { x, y -> a - 1 - (y - a * 2) }
-		cube[3].neighbours[up] = cube[4]
-		cube[3].facingTransform[up] = right
-		cube[3].xTransform[up] = { x, y -> a }
-		cube[3].yTransform[up] = { x, y -> a + x }
-
-		cube[4].cx = 1
-		cube[4].cy = 1
-		cube[4].neighbours[right] = cube[1]
-		cube[4].facingTransform[right] = up
-		cube[4].xTransform[right] = { x, y -> a * 2 + (y - a) }
-		cube[4].yTransform[right] = { x, y -> a - 1 }
-		cube[4].neighbours[down] = cube[2]
-		cube[4].facingTransform[down] = down
-		cube[4].xTransform[down] = { x, y -> x }
-		cube[4].yTransform[down] = { x, y -> y + 1 }
-		cube[4].neighbours[left] = cube[3]
-		cube[4].facingTransform[left] = down
-		cube[4].xTransform[left] = { x, y -> y - a }
-		cube[4].yTransform[left] = { x, y -> a * 2 }
-		cube[4].neighbours[up] = cube[0]
-		cube[4].facingTransform[up] = up
-		cube[4].xTransform[up] = { x, y -> x }
-		cube[4].yTransform[up] = { x, y -> y - 1 }
-
-		cube[5].cx = 0
-		cube[5].cy = 3
-		cube[5].neighbours[right] = cube[2]
-		cube[5].facingTransform[right] = up
-		cube[5].xTransform[right] = { x, y -> a + (y - a * 3) }
-		cube[5].yTransform[right] = { x, y -> a * 3 - 1 }
-		cube[5].neighbours[down] = cube[1]
-		cube[5].facingTransform[down] = down
-		cube[5].xTransform[down] = { x, y -> a * 2 + x }
-		cube[5].yTransform[down] = { x, y -> 0 }
-		cube[5].neighbours[left] = cube[0]
-		cube[5].facingTransform[left] = down
-		cube[5].xTransform[left] = { x, y -> a + (y - a * 3) }
-		cube[5].yTransform[left] = { x, y -> 0 }
-		cube[5].neighbours[up] = cube[3]
-		cube[5].facingTransform[up] = up
-		cube[5].xTransform[up] = { x, y -> x }
-		cube[5].yTransform[up] = { x, y -> y - 1 }
-
-		return cube
+	fun findStraightNeighboursBfs(faces: Map<Pair<Int, Int>, Face>, face0: Face) {
+		val queue = ArrayDeque<Face>()
+		queue.addLast(face0)
+		face0.vector = Point3D(0.0, 0.0, -1.0)
+		while (queue.isNotEmpty()) {
+			val curr = queue.removeFirst()
+			for ((facing, delta) in positionToMove) {
+				val (dx, dy) = delta
+				if (curr.neighbours[facing] !== curr) continue
+				val neighbour = faces[curr.cx + dx to curr.cy + dy] ?: continue
+				val revFacing = reverse[facing]!!
+				curr.neighbours[facing] = neighbour
+				curr.facing[facing] = facing
+				neighbour.neighbours[revFacing] = curr
+				neighbour.transform = rotateTransform(curr.transform, facing)
+				neighbour.vector = round(neighbour.transform.transform(face0.vector))
+				neighbour.facing[revFacing] = revFacing
+				queue.addLast(neighbour)
+			}
+		}
 	}
 
-	fun part2(input: List<String>, cube: List<Face>): Int {
+	fun findOtherNeighbours(faces: Map<Pair<Int, Int>, Face>, face0: Face) {
+		for (face in faces.values) {
+			for ((facing, _) in positionToMove) {
+				if (face.neighbours[facing] !== face) continue
+				val neighbourTransform = rotateTransform(face.transform, facing)
+				val neighbourVector = round(neighbourTransform.transform(face0.vector))
+				val neighbour = faces.values.find { it.vector == neighbourVector }!!
+				face.neighbours[facing] = neighbour
+			}
+		}
+	}
+
+	fun findNeighboursFacing(faces: Map<Pair<Int, Int>, Face>) {
+		for (face in faces.values) {
+			for ((facing, _) in positionToMove) {
+				val neighbour = face.neighbours[facing]
+				val neighbourFacing = neighbour.neighbours.withIndex().find { n -> n.value === face }!!.index
+				face.facing[facing] = reverse[neighbourFacing]!!
+				neighbour.facing[neighbourFacing] = reverse[facing]!!
+			}
+		}
+	}
+
+	fun getPointOnBorder(p: Point3D, facing: Int): Pair<Double, Double> {
+		return when (facing) {
+			right -> -1.0 to p.y
+			left -> 1.0 to p.y
+			up -> p.x to 1.0
+			down -> p.x to -1.0
+			else -> error("Unknown facing: $facing")
+		}
+	}
+
+	fun initFaces(map: List<String>, width: Int, height: Int, a: Int): Map<Pair<Int, Int>, Face> {
+		val cubeWidth = width / a
+		val cubeHeight = height / a
+		val faces = mutableMapOf<Pair<Int, Int>, Face>()
+		for (cy in 0 until cubeHeight)
+			for (cx in 0 until cubeWidth)
+				if (map[cy * a][cx * a] != ' ')
+					faces[cx to cy] = Face(cx, cy)
+		return faces
+	}
+
+	fun part2(input: List<String>): Int {
 		val map = parseMap(input)
 		val width = map[0].length
 		val height = map.size
 		val a = min(width, height) / 3
-		val faces = mutableMapOf<Pair<Int, Int>, Face>()
-		for (face in cube) faces[face.cx to face.cy] = face
+		val faces = initFaces(map, width, height, a)
+		val x0 = map[0].indexOf('.')
+		val face0 = faces[x0 / a to 0]!!
+		findStraightNeighboursBfs(faces, face0)
+		findOtherNeighbours(faces, face0)
+		findNeighboursFacing(faces)
+
+		val a2 = (a.toDouble() - 1) / 2
 		val path = input.last()
 		var facing = right
 		var x = map[0].indexOf('.')
@@ -350,21 +208,23 @@ fun main() {
 					val next = min(l, r)
 					val distance = path.substring(p ,next).toInt()
 					for (m in 0 until distance) {
-						val (dx, dy) = when (facing) {
-							right -> 1 to 0
-							down -> 0 to 1
-							left -> -1 to 0
-							up -> 0 to -1
-							else -> error("Unknown facing: $facing")
-						}
+						val (dx, dy) = positionToMove[facing]!!
 						var x2 = x + dx
 						var y2 = y + dy
 						var facing2 = facing
 						if (x2 !in 0 until width || y2 !in 0 until height || map[y2][x2] == ' ') {
-							val face = faces[(x / a) to (y / a)]!!
-							x2 = face.xTransform[facing](x, y)
-							y2 = face.yTransform[facing](x, y)
-							facing2 = face.facingTransform[facing]
+							val cx = x / a
+							val cy = y / a
+							val face = faces[cx to cy]!!
+							val face2 = face.neighbours[facing]
+							facing2 = face.facing[facing]
+							val x1 = (x % a).toDouble() / a2 - 1
+							val y1 = (y % a).toDouble() / a2 - 1
+							val p3d = face.transform.transform(Point3D(x1, y1, -1.0))
+							val pNearBorder = face2.transform.createInverse().transform(p3d)
+							val (xBorder, yBorder) = getPointOnBorder(pNearBorder, facing2)
+							x2 = face2.cx * a + ((xBorder + 1) * a2).roundToInt()
+							y2 = face2.cy * a + ((yBorder + 1) * a2).roundToInt()
 						}
 						if (map[y2][x2] == '.') {
 							x = x2
@@ -383,8 +243,8 @@ fun main() {
 
 	val testInput = readInput("Day22_test")
 	check(part1(testInput) == 6032)
-	check(part2(testInput, createTestCube()) == 5031)
+	check(part2(testInput) == 5031)
 	val input = readInput("Day22")
 	println(part1(input))
-	println(part2(input, createCube()))
+	println(part2(input))
 }
